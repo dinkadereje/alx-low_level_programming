b3bce934394dff05d75582cefdfd59a29709c4da
root <dinqaadarajjee@gmail.com>
2022-03-10 10:28:56 -0800

    First C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+/**
+ * main - This function runs first
+ *
+ * Return: Return integer numbers
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
