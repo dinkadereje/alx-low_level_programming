15535208dadf6b44008c59284e9da084226c1047
root <dinqaadarajjee@gmail.com>
2022-03-11 11:04:26 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This function runs first
+ *
+ * Return: Return integer numbers
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
