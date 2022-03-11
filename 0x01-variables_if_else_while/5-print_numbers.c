09cbca29236499af6341d57c7e15f52c6f9bfa58
root <dinqaadarajjee@gmail.com>
2022-03-11 10:43:49 -0800

    Numbers'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This function runs first
+ *
+ * Return: Return integer numbers
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
