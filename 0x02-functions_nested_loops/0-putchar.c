472e33e4f1086e9849d1e2a717b18e788f17ca71
root <dinqaadarajjee@gmail.com>
2022-03-15 10:43:40 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - This function runs first
+ *
+ * Return: Return integer numbers
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
