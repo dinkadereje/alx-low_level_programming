22c8c5ad3dab50fe42b72d99e1c38c6b435ac0ac
root <dinqaadarajjee@gmail.com>
2022-03-15 13:18:55 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - takes two variables
+ * @x: frist integer argument
+ * @y: second integer argument
+ * Return: void
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
